676047e7473d0e078a94ad593ca60261dadb2285
Alex Depatie <a.r.depatie@gmail.com>
2021-04-10 20:57:21 -0400

    dashboard + geocomplete styling google fonts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import styled from "styled-components";
 import useWeather from "../../hooks/useWeather";
+import WeatherProvider from "../../hooks/useWeather/WeatherProvider";
 import Geocomplete from "../Geocomplete";
 
 const DashboardContainer = styled.div`
-  width: 100%;
+  font-family: "Lexend", sans-serif;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
   padding-left: 20px;
   padding-right: 20px;
+  background: #2980b9; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #ffffff, #6dd5fa, #2980b9);
+  background: linear-gradient(to right, #ffffff, #6dd5fa, #2980b9);
 `;
 
 const ModalBackground = styled.div`
@@ -24,31 +33,40 @@ const ModalBackground = styled.div`
 const ModalWindow = styled.div`
   margin-top: 200px;
   max-width: 600px;
-  padding: 40px;
+  min-height: 70px;
+  padding: 60px 60px;
   background: rgba(255, 255, 255);
+  border-radius: 15px;
 `;
 
 function Dashboard() {
   const [location, setLocation] = useState({});
+  const [showLocationModal, setShowLocationModal] = useState(true);
   const {
     isLoading: isWeatherLoading,
     error: weatherError,
     data: weatherData,
     isIdle,
   } = useWeather(location);
+
+  function handleLocationChanged(newLocation) {
+    setLocation(newLocation);
+    setShowLocationModal(false);
+  }
+
   return (
-    <DashboardContainer>
-      {(!!location || !!location.longitude || !!location.lattitude) && (
-        <ModalBackground>
-          <ModalWindow>
-            <h3>Enter Location:</h3>
-            <Geocomplete
-              onLocationChanged={(location) => setLocation(location)}
-            />
-          </ModalWindow>
-        </ModalBackground>
-      )}
-    </DashboardContainer>
+    <WeatherProvider value={{ location }}>
+      <DashboardContainer>
+        {showLocationModal && (
+          <ModalBackground>
+            <ModalWindow>
+              <Geocomplete onLocationChanged={handleLocationChanged} />
+            </ModalWindow>
+          </ModalBackground>
+        )}
+        <h1></h1>
+      </DashboardContainer>
+    </WeatherProvider>
   );
 }
 
--- src/components/Geocomplete/Geocomplete.js
+++ src/components/Geocomplete/Geocomplete.js
@@ -1,7 +1,44 @@
 import { useEffect } from "react";
 import styled from "styled-components";
 
-const TextInput = styled.input``;
+const TextField = styled.div`
+  position: relative;
+`;
+const TextInput = styled.input`
+  background: 0;
+  border: 0;
+  outline: none;
+  width: 80vw;
+  max-width: 400px;
+  font-size: 1.5em;
+  transition: padding 0.3s 0.2s ease;
+  &:focus {
+    padding-bottom: 5px;
+  }
+  &:focus + .line {
+    &:after {
+      transform: scaleX(1);
+    }
+  }
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 3px;
+  position: absolute;
+  bottom: -8px;
+  background: #bdc3c7;
+  &:after {
+    content: " ";
+    position: absolute;
+    float: right;
+    width: 100%;
+    height: 3px;
+    transform: scalex(0);
+    transition: transform 0.3s ease;
+    background: #1abc9c;
+  }
+`;
 
 function Geocomplete({ onLocationChanged }) {
   useEffect(() => {
@@ -19,11 +56,17 @@ function Geocomplete({ onLocationChanged }) {
 
     autocomplete.addListener("place_changed", () => {
       const place = autocomplete.getPlace();
-      onLocationChanged(place.location);
+      console.log("PLACE", place);
+      onLocationChanged(place.geometry.location);
     });
   }, [onLocationChanged]);
 
-  return <TextInput id="address-field" />;
+  return (
+    <TextField>
+      <TextInput id="address-field" />
+      <Line />
+    </TextField>
+  );
 }
 
 export default Geocomplete;
